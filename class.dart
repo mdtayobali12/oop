efb7ccf24b95ec991c0fd054e36c36d85ac05552
mdtayobali12 <147986794+mdtayobali12@users.noreply.github.com>
2023-11-17 21:59:13 +0600

    first commit

---
+++ class.dart
@@ -0,0 +1,12 @@
+void main(){
+  student obj = student();
+  obj.name ="Md.Tayob alli";
+  obj.age = 25;
+  obj.addriss = "Dinajpur";
+  print(obj.name);
+}
+class student{
+  late String name ;
+  late int age ;
+  late String addriss ;
+}
